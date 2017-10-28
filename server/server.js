231d26f536a7145b28b531be7f9e084b2e7505c8
Aditya Narayan Shahi <adityan.shahi2014@gmail.com>
2017-10-29 02:01:29 +0530

    Added Socket IO

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 console.log('Starting app.js');
 
 const path=require('path')
+const http=require('http')
 const express=require('express')
+const socketIO = require('socket.io')
 
 const publicPath=path.join(__dirname,'../public')
 var app=express();
+var server=http.createServer(app);
+var io= socketIO(server);
 const port=process.env.PORT||3000;
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+server.listen(port,()=>{
   console.log(`Server started on port ${port}`);
-});
+}); 
