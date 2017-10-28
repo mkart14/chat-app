9c15693cd3f48605cdf1fad37bf95a4f621fdbb8
Aditya Narayan Shahi <adityan.shahi2014@gmail.com>
2017-10-28 23:48:06 +0530

    Added start page and created server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+console.log('Starting app.js');
+
+const path=require('path')
+const express=require('express')
+
+const publicPath=path.join(__dirname,'../public')
+var app=express();
+const port=process.env.PORT||3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port,()=>{
+  console.log(`Server started on port ${port}`);
+});
